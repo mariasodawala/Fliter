06fe29a53ef37142ee3866407a1cf90a7cde9e10
mariasodawala <106393060+mariasodawala@users.noreply.github.com>
2022-12-26 16:47:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    
+}
+
+function draw()
+    {
+
+    }
+    function take_snnapshot()
+    {
+    save('My_FilterImage.png')
+    }
+    
